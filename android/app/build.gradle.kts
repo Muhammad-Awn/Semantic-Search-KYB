f91adaa7afd55cbb075d06eb2455d1c1cccfe21e
Muhammad-Awn <muhammadawn24@gmail.com>
2025-04-21 02:18:47 +0500

    OCR addition

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.screenshot_detection_v1"
+    namespace = "com.example.KnowYourBooks"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.screenshot_detection_v1"
+        applicationId = "com.example.KnowYourBooks"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
